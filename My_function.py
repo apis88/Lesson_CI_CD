bc32ec6dd20fe0f2b2110497abcc26f14cba97e9
apis88 <apis88@bk.ru>
2022-11-13 20:47:21 +0300

    ДЗ CI/CD

---
+++ My_function.py
@@ -0,0 +1,6 @@
+def square(number):
+    """
+    this is my function
+    """
+    result = (number ** 2)
+    return result
--- My_test.py
+++ My_test.py
@@ -1,1 +1,22 @@
-print('Привет мир')
+from My_function  import square
+def test(number):
+    s = square(number)
+
+    test_s = number ** 2
+
+    assert s == test_s
+
+def test2(number):
+    s = square(number)
+
+    test_s2 = number * number
+
+    assert s == test_s2
+
+
+test(10)
+test(1)
+test(40)
+test2(10)
+test2(1)
+test2(40)
